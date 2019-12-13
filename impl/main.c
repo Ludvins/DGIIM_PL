32db6adf8dad69aa39d595ecfdaa5ce995031808
antcc <antoniocoin07@gmail.com>
2019-12-13 15:01:49 +0100

    Añade bloques iniciales; permite elegir salida"

--- impl/main.c
+++ impl/main.c
@@ -2,29 +2,52 @@
 #include <stdlib.h>
 
 extern FILE *yyin;
+FILE * fout;
 extern unsigned error;
 int yyparse();  // Para evitar warning al compilar
 
+const char * DEFAULT_OUT = "salida.c";
+
 int main(int argc, char * argv[]) {
-  if (argc >= 2) {
+  if (argc > 1) {
     yyin = fopen (argv[1], "rt");
     if (yyin == NULL) {
-      printf ("---> El fichero %s no se puede abrir\n", argv[1]);
+      fprintf (stderr, "---> El fichero %s no se puede abrir\n", argv[1]);
       exit (1);
     }
     else {
-      printf("---> Leyendo fichero '%s'...\n", argv[1]);
+      fprintf(stderr, "---> Leyendo fichero '%s'...\n", argv[1]);
     }
   }
   else {
-    printf("---> Leyendo entrada estándar...\n");
+    fprintf(stderr, "---> Leyendo entrada estándar...\n");
     yyin = stdin;
   }
 
+  fflush(stderr);
+
+  char nombre[100];
+
+  if (argc > 2) {
+    sprintf(nombre, argv[2]);
+  }
+  else {
+    sprintf(nombre, DEFAULT_OUT);
+  }
+  fout = fopen(nombre, "w");
+
   yyparse();
 
-  printf("---> Se ha llegado al final del programa.\n");
-  printf("Número de errores: %d.\n", error);
+  fprintf(stderr, "---> Se ha llegado al final del programa.\n");
+  fprintf(stderr, "Número de errores: %d.\n", error);
+
+  if (!error) {
+    char llamada[100];
+
+    fprintf(stderr, "Compilando programa...\n");
+    sprintf(llamada, "gcc %s -o salida.out", nombre);
+    system(llamada);
+  }
 
   return error;
 }
